5da5c74137a3195336655d87c056f927ebbcc9f7
raylu <90059+raylu@users.noreply.github.com>
2022-08-09 02:13:25 -0700

    don't press T on last position
    treat last position as (vaal) skill to leave on T

--- autoaura.py
+++ autoaura.py
@@ -18,14 +18,14 @@ def main():
 
 	with open('positions.json', 'r') as f:
 		aura_positions = json.load(f)
-	for pos in aura_positions:
+	for pos in aura_positions[:-1]:
 		activate_aura(mouse, keyboard, pos)
 
 	mouse.position = (1636, 1163)
 	time.sleep(0.05)
 	mouse.click(Button.left)
 	time.sleep(0.05)
-	mouse.position = (1393 + 1 * 72, 990 - 3 * 72) # impurity of lightning
+	mouse.position = (1393 + aura_positions[-1][0] * 72, 990 - aura_positions[-1][1] * 72) # last slot
 	time.sleep(0.05)
 	mouse.click(Button.left)
 	time.sleep(0.05)
@@ -46,7 +46,7 @@ def activate_aura(mouse, keyboard, pos):
 	time.sleep(0.05)
 
 def setup(mouse : pynput.mouse.Controller):
-	positions = set()
+	positions = []
 
 	def on_press(key):
 		nonlocal positions
@@ -55,12 +55,12 @@ def setup(mouse : pynput.mouse.Controller):
 		elif key == pynput.keyboard.Key.ctrl_l:
 			pos = ((mouse.position[0] - 1356) // 72, -(mouse.position[1] - 1038) // 72)
 			print('recording', pos)
-			positions.add(pos)
+			positions.append(pos)
 
 	with pynput.keyboard.Listener(on_press=on_press) as listener:
 		listener.join()
 	with open('positions.json', 'w') as f:
-		json.dump(list(positions), f, indent='\t')
+		json.dump(positions, f, indent='\t')
 
 if __name__ == '__main__':
 	main()
